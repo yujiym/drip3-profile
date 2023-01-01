70dcb861b7d4c201645985be56afa83da862edcf
Yuji Yamaguchi <yuji.ym@gmail.com>
2023-01-01 17:56:23 +0900

    update link-preview

--- packages/ui/components/Post.tsx
+++ packages/ui/components/Post.tsx
@@ -7,11 +7,25 @@ import { shortAddress } from '../store'
 dayjs.extend(relativeTime)
 
 export default function Post({ post }: { post: any }) {
+  const title: string = post.indexing_metadata?.urlMetadata?.title ?? post.content.body
+  const description: string = post.indexing_metadata?.urlMetadata?.description ?? ''
+  const image: string = post.indexing_metadata?.urlMetadata?.image ?? ''
+  const listStyle = image
+    ? {
+        backgroundImage: `url(${image})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'right center',
+        backgroundSize: 'contain'
+      }
+    : {}
+  console.log(listStyle)
+
   return (
-    <li className="border-b border-indigo-800/30 p-4">
+    <li className="border-b border-indigo-800/30 p-4" style={listStyle}>
       <a className="font-bold" href={post.content.body} target="_blank" rel="noreferrer noopener">
-        {post.content.body}
+        {title}
       </a>
+      <p className="text-sm">{description}</p>
       {post.content.data?.comment && <div className="flex items-center text-sm mt-2"></div>}
       <div className="flex items-center text-xs mt-1">
         <span className="w-3 h-3 bg-stripe border-primary border rounded-full" />
